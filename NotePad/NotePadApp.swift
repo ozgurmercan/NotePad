fc61e487a1ef66b2e12ee0e352be89e3bb45c65c
ÖZGÜR MERCAN <ozgurmercan021@gmail.com>
2023-05-11 18:21:15 +0300

    Initial Commit

---
+++ NotePad/NotePadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotePadApp.swift
+//  NotePad
+//
+//  Created by Özgür Mercan on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct NotePadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
